d86249ef10d5f2604734d495b24976084f154db6
Hemanth V. Alluri <hdrive1999@gmail.com>
2021-07-12 12:51:18 +0530

    gvimrc: Remove usage of gvimrc for the base installation
    If you'd like to also copy over the gvimrc, switch over to the extended
    branch.

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ for opt in $@; do
 	case "${opt}" in
 		"--clean")
 			echo "Performing a clean installation."
-			rm -rdf ~/.vim ~/.vimrc ~/.gvimrc
+			rm -rdf ~/.vim ~/.vimrc
 			;;
 		"--update")
 			echo "Updating packages."
@@ -21,7 +21,6 @@ for opt in $@; do
 done
 
 cp vimrc ~/.vimrc
-cp gvimrc ~/.gvimrc
 cp -rT vim ~/.vim
 
 if [ "${update}" = true ]; then
